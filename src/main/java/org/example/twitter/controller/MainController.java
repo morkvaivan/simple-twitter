13b73e5e253fbe6ca8f660c2e2804b2860d3ce2a
morkvaivan <morkvaivan@icloud.com>
2020-03-04 11:42:04 +0200

    [feature/link-user-to-messages]: Linked user to messages.

--- src/main/java/org/example/twitter/controller/MainController.java
+++ src/main/java/org/example/twitter/controller/MainController.java
@@ -1,14 +1,15 @@
 package org.example.twitter.controller;
 
 import org.example.twitter.domain.Message;
+import org.example.twitter.domain.User;
 import org.example.twitter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,8 +32,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
